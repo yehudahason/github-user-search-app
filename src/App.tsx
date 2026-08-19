6a51594efa3b556206459f2b87f6241cea83d783
Yehuda Hason <yehudaxt@gmail.com>
2026-08-19 15:20:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1 className="text-amber-700">Home</h1>
+      </header>
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+}
